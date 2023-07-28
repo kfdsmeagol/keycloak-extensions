a173daefa193a56ef8558739781d4a1ae75cdc74
kdbits <90309004+kdbits@users.noreply.github.com>
2023-07-28 15:32:32 +1000

    Hello world app

---
+++ src/main/java/com/xrev/kc/DemoProvider.java
@@ -0,0 +1,8 @@
+package com.xrev.kc;
+
+import org.keycloak.provider.Provider;
+
+public interface DemoProvider extends Provider {
+
+    void sayHi();
+}
---
+++ src/main/java/com/xrev/kc/DemoProviderFactory.java
@@ -0,0 +1,6 @@
+package com.xrev.kc;
+
+import org.keycloak.provider.ProviderFactory;
+
+public interface DemoProviderFactory extends ProviderFactory<DemoProvider> {
+}
